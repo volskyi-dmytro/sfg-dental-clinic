1eaaa510134913dfbcb318263603f04a25a7f524
volskyi.dmytro <volskyi.dmytro@gmail.com>
2020-03-26 14:32:36 +0200

    Testing push to git

--- src/main/java/com/stpunk47/sfgdentalclinic/SfgDentalClinicApplication.java
+++ src/main/java/com/stpunk47/sfgdentalclinic/SfgDentalClinicApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SfgDentalClinicApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SfgDentalClinicApplication.class, args);
+        System.out.println("Some word");
     }
 
 }
