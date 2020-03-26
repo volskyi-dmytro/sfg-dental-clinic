354b5e87ec86bf081efc7875ea7356cc241cb0de
volskyi.dmytro <volskyi.dmytro@gmail.com>
2020-03-26 14:24:37 +0200

    First download from IntelliJ

---
+++ src/main/java/com/stpunk47/sfgdentalclinic/SfgDentalClinicApplication.java
@@ -0,0 +1,13 @@
+package com.stpunk47.sfgdentalclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDentalClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgDentalClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stpunk47/sfgdentalclinic/SfgDentalClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stpunk47.sfgdentalclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDentalClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
