de5d618f2553f0006dabaa9b6581405ab9237ea8
volskyi.dmytro <volskyi.dmytro@gmail.com>
2020-04-04 18:59:45 +0300

    List companies on appropriate index page

--- dental-clinic-web/src/main/java/com/stpunk47/sfgdentalclinic/controllers/CompaniesController.java
+++ dental-clinic-web/src/main/java/com/stpunk47/sfgdentalclinic/controllers/CompaniesController.java
@@ -1,14 +1,24 @@
 package com.stpunk47.sfgdentalclinic.controllers;
 
+import com.stpunk47.sfgdentalclinic.services.CompanyService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/companies")
 @Controller
 public class CompaniesController {
 
+    private final CompanyService companyService;
+
+    public CompaniesController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
     @RequestMapping({"","/","/index", "/index.html"})
-    public String listCompanies(){
+    public String listCompanies(Model model){
+        model.addAttribute("companies", companyService.findAll());
+
 
         return "/companies/index";
     }
