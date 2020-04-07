edab4598a8c0343a1f874c1e50d5e31b7f9c6ffe
volskyi.dmytro <volskyi.dmytro@gmail.com>
2020-04-07 14:50:14 +0300

    Added Department Map Service. Closes #28

---
+++ dental-clinic-data/src/main/java/com/stpunk47/sfgdentalclinic/services/DepartmentService.java
@@ -0,0 +1,8 @@
+package com.stpunk47.sfgdentalclinic.services;
+
+
+import com.stpunk47.sfgdentalclinic.model.Department;
+
+public interface DepartmentService extends CrudService<Department, Long> {
+
+}
---
+++ dental-clinic-data/src/main/java/com/stpunk47/sfgdentalclinic/services/map/DepartmentMapService.java
@@ -0,0 +1,37 @@
+package com.stpunk47.sfgdentalclinic.services.map;
+
+import com.stpunk47.sfgdentalclinic.model.Department;
+import com.stpunk47.sfgdentalclinic.services.DepartmentService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class DepartmentMapService extends AbsrtactMapService<Department, Long> implements DepartmentService {
+
+    @Override
+    public Set<Department> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Department findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Department save(Department object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Department object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteById(id);
+    }
+}
