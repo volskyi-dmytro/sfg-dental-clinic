7cb0aa9ad83e6168af63bb6f61aa1f0e1a04713c
volskyi.dmytro <volskyi.dmytro@gmail.com>
2020-04-07 14:31:58 +0300

    Added specialties. Added to Dentist. Closes #26

--- dental-clinic-data/src/main/java/com/stpunk47/sfgdentalclinic/model/Dentist.java
+++ dental-clinic-data/src/main/java/com/stpunk47/sfgdentalclinic/model/Dentist.java
@@ -1,4 +1,16 @@
 package com.stpunk47.sfgdentalclinic.model;
 
+import java.util.Set;
+
 public class Dentist extends Person {
+
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
