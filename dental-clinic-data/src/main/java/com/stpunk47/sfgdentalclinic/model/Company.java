76accf953f0ada7d2044bb60bd86465a10618151
volskyi.dmytro <volskyi.dmytro@gmail.com>
2020-04-07 14:36:12 +0300

    Added Contact info to Company. Closes #27

--- dental-clinic-data/src/main/java/com/stpunk47/sfgdentalclinic/model/Company.java
+++ dental-clinic-data/src/main/java/com/stpunk47/sfgdentalclinic/model/Company.java
@@ -4,7 +4,34 @@ import java.util.Set;
 
 public class Company extends Person {
 
+    private String address;
+    private String city;
+    private String phone;
     private Set<Worker> workers;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
     public Set<Worker> getWorkers() {
         return workers;
