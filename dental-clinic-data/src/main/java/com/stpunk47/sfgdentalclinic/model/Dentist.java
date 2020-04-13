03a44970fafee6a1310c1b6792ec8d60b24f29b0
volskyi.dmytro <volskyi.dmytro@gmail.com>
2020-04-13 20:09:24 +0300

    Convert Dentists to JPA Entities. Closes #36

--- dental-clinic-data/src/main/java/com/stpunk47/sfgdentalclinic/model/Dentist.java
+++ dental-clinic-data/src/main/java/com/stpunk47/sfgdentalclinic/model/Dentist.java
@@ -1,10 +1,17 @@
 package com.stpunk47.sfgdentalclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "dentists")
 public class Dentist extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "dentist_specialties",
+            joinColumns = @JoinColumn(name = "dentist_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
