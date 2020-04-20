56be9f33462bb3efb669212aef4c521ebd576762
volskyi.dmytro <volskyi.dmytro@gmail.com>
2020-04-20 15:11:55 +0300

    Implemented Map impl for Visit Service. Closes #45.

---
+++ dental-clinic-data/src/main/java/com/stpunk47/sfgdentalclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.stpunk47.sfgdentalclinic.services;
+
+import com.stpunk47.sfgdentalclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ dental-clinic-data/src/main/java/com/stpunk47/sfgdentalclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,41 @@
+package com.stpunk47.sfgdentalclinic.services.map;
+
+import com.stpunk47.sfgdentalclinic.model.Visit;
+import com.stpunk47.sfgdentalclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbsrtactMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if(visit.getWorker() == null || visit.getWorker().getCompany() == null || visit.getWorker().getId() ==null
+        || visit.getWorker().getCompany().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteByID(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
