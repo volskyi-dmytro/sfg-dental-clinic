60361979c4dbf8f2edc8b28948f5efb1ab13c232
volskyi.dmytro <volskyi.dmytro@gmail.com>
2020-04-30 14:15:47 +0300

    Upgrade to JUnit 5. Closes #4.

--- dental-clinic-web/src/test/java/com/stpunk47/sfgdentalclinic/SfgDentalClinicApplicationTests.java
+++ dental-clinic-web/src/test/java/com/stpunk47/sfgdentalclinic/SfgDentalClinicApplicationTests.java
@@ -2,11 +2,13 @@ package com.stpunk47.sfgdentalclinic;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
 @SpringBootTest(classes = com.stpunk47.sfgdentalclinic.SfgDentalClinicApplication.class)
-
+@ExtendWith(SpringExtension.class)
 class SfgDentalClinicApplicationTests {
 
     @Test
